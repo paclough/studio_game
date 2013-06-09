b62bfe307065ef6afa7660c54d8da3f488244774
pc <paclough@github>
2013-06-09 13:37:39 -0500

    Completed Pragmatic Studio / Ruby Programming through Chapter(?) 6 - Methods.

--- studio_game.rb
+++ studio_game.rb
@@ -1,26 +1,14 @@
-player1_name   = "larry"
-player1_health = 60
-player2_name   = "curly"
-player2_health = 125
-player3_name   = "moe"
-player3_health = 100
-player4_name   = "shemp"
-player4_health = 90
+def say_hello( player_name, player_health = 100 )
+  "I'm #{player_name.capitalize} with a health of #{player_health} as of #{time}."
+end
+
+def time
+  current_time = Time.new
+  current_time.strftime( "%I:%M:%S" )
+end
 
 
-puts "#{player1_name.capitalize} has a health of #{player1_health}."
-puts "#{player2_name.upcase} has a health of #{player2_health}."
-
-player2_health = player1_health
-puts "#{player2_name.upcase} has a health of #{player2_health}."
-
-player1_health = 30
-puts "#{player1_name.capitalize} has a health of #{player1_health}."
-puts "#{player2_name.upcase} has a health of #{player2_health}."
-puts "#{player3_name.capitalize} has a health of #{player3_health}.".center( 50, '*' )
-puts "#{player4_name.capitalize.ljust( 30, '.' )} #{player4_health} health"
-
-game_start = Time.new
-puts "The game started on #{game_start.strftime( "%A %D at %I:%M%p" ) }"
-
-# puts "Players:\n\t#{player1_name}\n\t#{player2_name}\n\t#{player3_name}"
+puts say_hello( "larry", 60 )
+puts say_hello( "curly", 125 )
+puts say_hello( "moe" )
+puts say_hello( "shemp", 90 )
