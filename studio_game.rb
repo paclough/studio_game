244a3b232f5fbcb83ef1dcfe1a2c1682767dd5ed
pc <paclough@github>
2013-06-08 22:35:54 -0500

    Initial commit for Pragmatic Studio ruby course. Started game project with some simple string & number manipulation.

---
+++ studio_game.rb
@@ -0,0 +1,9 @@
+player1_name = "larry"
+player1_health = 60
+player2_name = "curly"
+player3_name = "moe"
+
+
+puts "#{player1_name}'s health is #{player1_health / 9}"
+
+puts "Players:\n\t#{player1_name}\n\t#{player2_name}\n\t#{player3_name}"
