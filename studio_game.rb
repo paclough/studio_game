4f72db84424a2f571a5bea6293573b537a400924
pc <paclough@github>
2013-06-11 17:40:57 -0500

    Completed Pragmatic Studio / Ruby Programming through Chapter(?) 8 - Attributes.

--- studio_game.rb
+++ studio_game.rb
@@ -1,12 +1,19 @@
 class Player
 
+  attr_accessor :name
+  attr_reader   :health
+
   def initialize( name, health = 100 )
     @name = name.capitalize
     @health = health
   end
+
+  def name=( new_name )
+    @name = new_name.capitalize
+  end
   
   def to_s
-    "I'm #{@name} with a health of #{@health}."
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 
   def blam
@@ -18,18 +25,17 @@ class Player
     @health += 15
     puts "#{@name} got w00ted!"
   end
+
+  def score
+    @name.length + @health
+  end
   
 end
 
-player1 = Player.new( "moe" )
-puts player1
-
 player2 = Player.new( "larry", 60 )
+puts player2.name
+player2.name = "lawrence"
+puts player2.name
+puts player2.health
+puts player2.score
 puts player2
-
-player3 = Player.new( "curly", 125 )
-puts player3
-player3.blam
-puts player3
-player3.w00t
-puts player3
