4a7d737d4b33c32399d11ed6e6f61157a4b11f37
pc <paclough@github>
2013-06-12 23:23:37 -0500

    Completed Pragmatic Studio / Ruby Programming through Chapter(?) 10 - Objects Interacting.

--- studio_game.rb
+++ studio_game.rb
@@ -32,31 +32,52 @@ class Player
   
 end
 
+class Game
+
+  attr_reader :game_name
+
+  def initialize( name )
+    @game_name = name
+    @players = []
+  end
+
+  def add_player( player )
+    @players.push( player )
+  end
+
+  def play
+    puts "There are #{@players.size} players in #{@game_name}:"
+    @players.each { |player| puts player }
+
+    @players.each do |player|
+      rounds = 1 + rand( 4 )
+      rounds.times do
+        blam_or_w00t = rand( 10 )
+        if blam_or_w00t < 5
+          player.blam
+        else
+          player.w00t
+        end
+      end
+      puts player
+    end
+
+  end
+
+end
+
 player1 = Player.new( "moe" )
 player2 = Player.new( "larry", 60 )
 player3 = Player.new( "curly", 125 )
 
-players = [ player1, player2, player3]
+knuckleheads = Game.new( "Knuckleheads" )
+knuckleheads.add_player( player1 )
+knuckleheads.add_player( player2 )
+knuckleheads.add_player( player3 )
+knuckleheads.play
 
-players.pop
-player4 = Player.new( "shemp", 90 )
-players.push( player4 )
-
-puts "There are #{players.size} players in the game:"
-players.each { |player| puts player }
-
-players.each { |player| puts player.health }
-
-players.each do |player|
-  rounds = 1 + rand( 4 )
-  rounds.times do
-    blam_or_w00t = rand( 10 )
-    if blam_or_w00t < 5
-      player.blam
-    else
-      player.w00t
-    end
-  end
-  puts player
-end
-
+chipmunks = Game.new( "Chipmunks" )
+chipmunks.add_player( Player.new( "alvin" ) )
+chipmunks.add_player( Player.new( "simon", 80 ) )
+chipmunks.add_player( Player.new( "theodore", 77 ) )
+chipmunks.play
