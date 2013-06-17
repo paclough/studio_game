ebe9a304d7d31187c3d1a3b6d29a1812659fc9be
pc <paclough@github>
2013-06-16 23:27:12 -0500

    Almost completed Pragmatic Studio / Ruby Programming through Chapter(?) 18 - Custom Iterators ... missing the Bonus Round.

--- game.rb
+++ game.rb
@@ -57,8 +57,11 @@ class Game
 
     puts "\n#{total_points} total points from treasures found"
 
-    @players.each do |player|
+    @players.sort.each do |player|
       puts "\n#{player.name}'s point totals:"
+      player.each_found_treasure do |treasure|
+        puts "#{treasure.points} total #{treasure.name} points"
+      end
       puts "#{player.points} grand total points"
     end
 
