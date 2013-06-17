e31d423b1134583698799e78d53ca9c3c71d0761
pac913 <pac913@KIRTASPROC01>
2013-06-17 17:33:07 -0500

    Almost completed Pragmatic Studio / Ruby Programming through Chapter(?) 20 - Inheritance ... missing the Bonus Round.

--- studio_game.rb
+++ studio_game.rb
@@ -1,4 +1,6 @@
-require_relative "game"
+require_relative 'game'
+require_relative 'clumsy_player'
+require_relative 'berserk_player'
 
 player1 = Player.new( "moe" )
 player2 = Player.new( "larry", 60 )
@@ -10,6 +12,12 @@ knuckleheads.load_players( ARGV.shift || "players.csv" )
 # knuckleheads.add_player( player1 )
 # knuckleheads.add_player( player2 )
 # knuckleheads.add_player( player3 )
+
+klutz = ClumsyPlayer.new("klutz", 105)
+knuckleheads.add_player( klutz )
+
+berserker = BerserkPlayer.new("berserker", 50)
+knuckleheads.add_player( berserker )
 
 loop do
   puts "\nHow many rounds would you like to play? ('quit' to exit)"
